26752301ae67c41c5b49f9bc9d97ff02458d43f9
DLop1757 <danlopez1757@gmail.com>
2021-09-11 14:39:26 -0500

    Updates

---
+++ precache-manifest.15e9b70e1efb1d71299c013e11021ff6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b9f968bcd704bf059d0c5ad05d8dda0",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "62b63f49201a83a2ccbc",
+    "url": "/home/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "88448ffcfce5fdaf8e09",
+    "url": "/home/static/js/2.cfafeff4.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.cfafeff4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62b63f49201a83a2ccbc",
+    "url": "/home/static/js/main.1bf4547a.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "7881a287b006668589172ffd59c4a359",
+    "url": "/home/static/media/headshot.7881a287.png"
+  },
+  {
+    "revision": "108772991fc1104271a779f4a0c1645e",
+    "url": "/home/static/media/resume.10877299.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.df84cd9571af5c9c3b4e97876ee49e51.js"
+  "/home/precache-manifest.15e9b70e1efb1d71299c013e11021ff6.js"
 );
 
 self.addEventListener('message', (event) => {
